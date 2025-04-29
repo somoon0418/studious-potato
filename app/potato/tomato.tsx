b1fc043bf5721baaacfc7f3e3de85d1782489395
somoon0418 <somoon0418@gmail.com>
2025-04-29 21:47:40 +0900

    1.2: Root File, 1.3: Layout Component

--- app/potato/tomato.tsx
+++ app/potato/tomato.tsx
@@ -1,5 +1,16 @@
+import type { Route } from "../+types/root";
+
 export default function Tomato(){
     return (
         <div>Tomato</div>
     )
 }
+
+export const links: Route.LinksFunction = () => [
+    {rel: "stylesheet", href: "potato.com"}
+]
+
+export const meta: Route.MetaFunction = () => [
+    {title: "Tomato"},
+    {description: "Tomato is a delicious fruit"}
+]
