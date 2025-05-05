d50d5b7f66a586c52524f310e4b1e0fd164f484a
somoon0418 <somoon0418@gmail.com>
2025-05-05 21:48:34 +0900

    3.8: Job Card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -4,6 +4,16 @@ import { Button } from "../components/ui/button";
 import { Link } from "react-router";
 import { PostCard } from "~/features/community/components/post-card";
 import { IdeaCard } from "~/features/ideas/components/idea-card";
+import { JobCard } from "~/features/jobs/components/job-card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "../components/ui/card";
+import { Badge } from "../components/ui/badge";
 
 export const meta: MetaFunction = () => {
   return [
@@ -81,6 +91,33 @@ export default function HomePage() {
             postedAt="12 hours ago"
             likeCount={12}
             claimed={index % 2 === 0}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Latest Jobs
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            Find your dream job.
+          </p>
+          <Button asChild variant="link" className="text-lg p-0">
+            <Link to="/jobs">Explore all jobs &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <JobCard
+            key={`jobId-${index}`}
+            id={`jobId-${index}`}
+            company="Tesla"
+            companyLogoUrl="https://github.com/facebook.png"
+            companyHq="San Francisco, CA"
+            title="Software Engineer"
+            postedAt="12 hours ago"
+            type="Full-time"
+            positionLocation="Remote"
+            salary="$100,000 - $120,000"
           />
         ))}
       </div>
