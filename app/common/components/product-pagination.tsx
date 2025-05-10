e2826196a98ec46324a60476b2e0f860bf40f6be
somoon0418 <somoon0418@gmail.com>
2025-05-10 12:35:20 +0900

    3.17: Pagination

--- app/common/components/product-pagination.tsx
+++ app/common/components/product-pagination.tsx
@@ -1,3 +1,14 @@
+import { useSearchParams } from "react-router";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationLink,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationEllipsis,
+  PaginationNext,
+} from "./ui/pagination";
+import { data } from "react-router";
 type ProductPaginationProps = {
   totalPages: number;
 };
@@ -5,5 +16,53 @@ type ProductPaginationProps = {
 export default function ProductPagination({
   totalPages,
 }: ProductPaginationProps) {
-  return <div>ProductPagination</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get("page") ?? 1);
+
+  const getUrlWithPage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", String(page));
+    return `?${params}`;
+  };
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        {page !== 1 && (
+          <>
+            <PaginationItem>
+              <PaginationPrevious to={getUrlWithPage(page - 1)} />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink to={getUrlWithPage(page - 1)}>
+                {page - 1}
+              </PaginationLink>
+            </PaginationItem>
+          </>
+        )}
+        <PaginationItem>
+          <PaginationLink to={getUrlWithPage(page)} isActive>
+            {page}
+          </PaginationLink>
+        </PaginationItem>
+        {page !== totalPages && (
+          <>
+            <PaginationItem>
+              <PaginationLink to={getUrlWithPage(page + 1)}>
+                {page + 1}
+              </PaginationLink>
+            </PaginationItem>
+            {page + 1 !== totalPages && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+            <PaginationItem>
+              <PaginationNext to={getUrlWithPage(page + 1)} />
+            </PaginationItem>
+          </>
+        )}
+      </PaginationContent>
+    </Pagination>
+  );
 }
