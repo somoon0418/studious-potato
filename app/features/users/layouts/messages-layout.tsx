59452b0f8e0ddc50b67acd279898e3e4ca4c33c7
somoon0418 <somoon0418@gmail.com>
2025-05-24 11:30:37 +0900

    3.44: Message Page

--- app/features/users/layouts/messages-layout.tsx
+++ app/features/users/layouts/messages-layout.tsx
@@ -1,23 +1,16 @@
 import { Outlet } from "react-router";
 import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "~/common/components/ui/avatar";
-import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
   SidebarProvider,
 } from "~/common/components/ui/sidebar";
 import { MessageCard } from "../components/message-card";
 
 export default function ProfileLayout() {
   return (
-    <SidebarProvider className="max-h-[calc(100vh-14rem)] overflow-hidden h-full min-h-full">
+    <SidebarProvider className="flex max-h-[calc(100vh-14rem)] overflow-hidden h-[calc(100vh-14rem)] min-h-full">
       <Sidebar className="pt-16" variant="floating">
         <SidebarContent>
           <SidebarGroup>
