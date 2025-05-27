3b8f55eef09fd21cbb79c9c15e0fd77b6408b6c1
somoon0418 <somoon0418@gmail.com>
2025-05-27 17:04:31 +0900

    4.43: Profile Tables

--- app/features/users/schema.ts
+++ app/features/users/schema.ts
@@ -1,1 +1,52 @@
 //all the models for the users feature
+
+import {
+  bigint,
+  jsonb,
+  pgEnum,
+  pgSchema,
+  pgTable,
+  uuid,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+const users = pgSchema("auth").table("users", {
+  id: uuid().primaryKey(),
+});
+
+export const roles = pgEnum("roles", [
+  "developer",
+  "designer",
+  "marketer",
+  "founder",
+  "product-manager",
+]);
+export const profiles = pgTable("profiles", {
+  profile_id: uuid()
+    .primaryKey()
+    .references(() => users.id, { onDelete: "cascade" }),
+  avatar: text(),
+  name: text().notNull(),
+  username: text().notNull(),
+  headline: text(),
+  bio: text(),
+  role: roles().default("developer").notNull(),
+  stats: jsonb().$type<{
+    followers: number;
+    following: number;
+  }>(),
+  views: jsonb(),
+  created_at: timestamp().notNull().defaultNow(),
+  updated_at: timestamp().notNull().defaultNow(),
+});
+
+export const follows = pgTable("follows", {
+  follower_id: uuid().references(() => profiles.profile_id, {
+    onDelete: "cascade",
+  }),
+  following_id: uuid().references(() => profiles.profile_id, {
+    onDelete: "cascade",
+  }),
+  created_at: timestamp().notNull().defaultNow(),
+});
