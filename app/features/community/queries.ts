ae889499eb2c6acba5b1eeefadf24784ce448c52
somoon0418 <somoon0418@gmail.com>
2025-05-30 11:22:29 +0900

    5.3: Using Supabase Client

--- app/features/community/queries.ts
+++ app/features/community/queries.ts
@@ -2,12 +2,33 @@ import client from "~/supa-client";
 
 export const getTopics = async () => {
   const { data, error } = await client.from("topics").select("name, slug");
+  if (error) throw new Error(error.message);
   return data;
+};
+
+type PostWithRelations = {
+  post_id: number;
+  title: string;
+  created_at: string;
+  topic: {
+    name: string;
+  };
+  author: {
+    name: string;
+    username: string;
+    avatar?: string;
+  };
+  upvotes: {
+    count: number;
+  }[];
 };
 
 export const getPosts = async () => {
   const { data, error } = await client
     .from("posts")
-    .select(`id, title, created_at`);
-  return data;
+    .select(
+      `post_id, title, created_at, topic:topic_id!inner(name), author:profile_id!inner(name, username, avatar), upvotes:post_upvotes(count)`
+    );
+  if (error) throw new Error(error.message);
+  return <PostWithRelations[]>data;
 };
