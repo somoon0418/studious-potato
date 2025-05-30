a27b15251bcb9b3adeaa937a1919b6912c81fdfd
somoon0418 <somoon0418@gmail.com>
2025-05-30 15:44:45 +0900

    5.5: Type Fest

--- app/features/community/pages/community-page.tsx
+++ app/features/community/pages/community-page.tsx
@@ -101,14 +101,14 @@ export default function CommunityPage({ loaderData }: Route.ComponentProps) {
           <div className="space-y-5">
             {loaderData.posts.map((post) => (
               <PostCard
-                key={post.post_id!}
-                id={post.post_id!}
-                title={post.title!}
-                author={post.author!}
+                key={post.post_id}
+                id={post.post_id}
+                title={post.title}
+                author={post.author}
                 authorAvatarUrl={post.author_avatar}
-                category={post.topic!}
-                postedAt={post.created_at!}
-                votesCount={post.upvotes!}
+                category={post.topic}
+                postedAt={post.created_at}
+                votesCount={post.upvotes}
                 expanded
               />
             ))}
